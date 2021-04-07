9fd72f755a274d9bd7ad76223150ac449d7d8a62
Fernando Heisser <fernando_heisser@dellteam.com>
2021-04-07 19:50:13 -0300

    sql queries updated v2

--- src/database/carpoolScripts.sql
+++ src/database/carpoolScripts.sql
@@ -95,18 +95,29 @@ DELETE FROM carpools_offered WHERE id = ;
 
 DELETE FROM carpool_match WHERE id = ;
 
+INSERT INTO users (email, name, cpf, password, phone, city, neighborhood, street, facebook, instagram, twitter, photo, carpool_done, carpool_canceled, carpool_offered, carpool_requested)
+VALUES 	( 'fernando@email.com', 'Fernando Heisser', '04310299032', 'password', '51995258425', 'city', 'neighborhood', 'street', 'facebook', 'instagram', 'twitter', 'photo', 0, 0, 0, 0);
+
+INSERT INTO users (email, name, cpf, password, phone, city, neighborhood, street, facebook, instagram, twitter, photo, carpool_done, carpool_canceled, carpool_offered, carpool_requested)
+VALUES 	( 'nanando@email.com', 'Nanando Fixa', '14310299033', 'password', '55995258425', 'city', 'neighborhood', 'street', 'facebook', 'instagram', 'twitter', 'photo', 0, 0, 0, 0);
+
+INSERT INTO carpools_offered (user_id, phone, from_city, from_neighborhood, from_street, to_city, to_neighborhood, to_street, start_date, end_date, available_vacancies, canceled, done)
+VALUES 	( 1, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-03-31 10:15:00', '2021-03-31 10:30:00', 4, 0, 0),
+		( 1, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-03-29 10:15:00', '2021-03-29 10:30:00', 4, 0, 0),
+		( 1, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-04-10 10:15:00', '2021-04-10 10:30:00', 4, 0, 0),
+		( 1, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-04-15 10:15:00', '2021-04-15 10:30:00', 4, 0, 0),
+		( 1, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-04-12 10:15:00', '2021-04-12 10:30:00', 4, 0, 0);
 
 INSERT INTO carpools_requested (user_id, phone, from_city, from_neighborhood, from_street, to_city, to_neighborhood, to_street, start_date, end_date, canceled, done)
-VALUES ( 1, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-03-31 10:15:00', '2021-03-31 10:30:00', 0, 0);
-
-INSERT INTO carpools_requested (user_id, phone, from_city, from_neighborhood, from_street, to_city, to_neighborhood, to_street, start_date, end_date, canceled, done)
-VALUES ( 1, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-03-29 10:15:00', '2021-03-29 10:30:00', 0, 0);
-
-INSERT INTO carpools_requested (user_id, phone, from_city, from_neighborhood, from_street, to_city, to_neighborhood, to_street, start_date, end_date, canceled, done)
-VALUES ( 1, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-04-10 10:15:00', '2021-04-10 10:30:00', 0, 0);
-
-INSERT INTO carpools_requested (user_id, phone, from_city, from_neighborhood, from_street, to_city, to_neighborhood, to_street, start_date, end_date, canceled, done)
-VALUES ( 1, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-04-15 10:15:00', '2021-04-15 10:30:00', 0, 0);
-
-INSERT INTO carpools_requested (user_id, phone, from_city, from_neighborhood, from_street, to_city, to_neighborhood, to_street, start_date, end_date, canceled, done)
-VALUES ( 1, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-04-12 10:15:00', '2021-04-12 10:30:00', 0, 0);
+VALUES 	( 2, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-03-31 10:15:00', '2021-03-31 10:30:00', 0, 0),
+		( 2, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-03-29 10:15:00', '2021-03-29 10:30:00', 0, 0),
+		( 2, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-04-10 10:15:00', '2021-04-10 10:30:00', 0, 0),
+		( 2, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-04-15 10:15:00', '2021-04-15 10:30:00', 0, 0),
+		( 2, '12345678900', 'A', 'B', 'C', 'D', 'E', 'F', '2021-04-12 10:15:00', '2021-04-12 10:30:00', 0, 0);
+        
+INSERT INTO carpool_match (carpool_request_id, carpool_offer_id, accepted)
+VALUES 	( 1, 1, 0),
+		( 2, 2, 0),
+		( 3, 3, 0),
+		( 4, 4, 0),
+		( 4, 5, 0);
