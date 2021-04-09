d9972d433cb779c09ab8fc604c8c6b5149488fc9
Fernando Heisser <fernando_heisser@dellteam.com>
2021-04-08 23:21:20 -0300

    index.php get test

--- src/index.php
+++ src/index.php
@@ -7,83 +7,21 @@
     </head>
     <body>
         <?php
-            // include_once("repositories/CarpoolRequestRepository.class.php");
-            // include_once("repositories/CarpoolOfferRepository.class.php");
-            // include_once("repositories/CarpoolMatchRepository.class.php");
-            // include_once("repositories/UserRepository.class.php");
-            // include_once("models/CarpoolRequest.class.php");
-            // include_once("models/CarpoolOffer.class.php");
-            // include_once("models/CarpoolMatch.class.php");
-            // include_once("models/User.class.php");
+            include_once("repositories/CarpoolRequestRepository.class.php");
+            include_once("repositories/CarpoolOfferRepository.class.php");
+            include_once("repositories/CarpoolMatchRepository.class.php");
+            include_once("repositories/UserRepository.class.php");
+            $userRepository = new UserRepository();
+            echo($userRepository->getUsers());
 
-            // $user1 = new User(
-            //     'fernando.heisserch@gmail.com',
-            //     'Fernando Costa Heisser',
-            //     '04310299032',
-            //     '123',
-            //     '51995258425',
-            // );
-            // $user1->setCity('Charqueadas');
-            // $user1->setNeighborhood('Centro');
-            // $user1->setStreet('Patricio Ferreira');
-            // $user1->setFacebook('https://facebook.com/fernando.heisser/');
-            // $user1->setInstagram('https://instagram.com/fernando_heisser/');
-            // $user1->setTwitter('https://twitter.com/fernandoheisser');
-            
-            // $user2 = new User(
-            //     "nanandofixa@gmail.com",
-            //     "Nanando Fixa",
-            //     "04310299031",
-            //     "123",
-            //     "51995258424"
-            // );
-            
-            // $userRepository = new UserRepository();
-            // $user1Id = $userRepository->createUser($user1);
-            // $user2Id = $userRepository->createUser($user2);
-            
-            // echo($userRepository->getUsers());
+            $carpoolRequestRepository = new CarpoolRequestRepository();
+            //echo($carpoolRequestRepository->getCarpoolRequests());
 
-            // $carpoolRequest = new CarpoolRequest(
-            //     $user1Id,
-            //     '51995258425',
-            //     'Charqueadas',
-            //     'Centro',
-            //     'Patricio Ferreira',
-            //     'Charqueadas',
-            //     'Centro',
-            //     'Rua de tal',
-            //     '2021-03-31 10:15:00',
-            //     '2021-03-31 10:30:00'
-            // );
+            $carpoolOfferRepository = new CarpoolOfferRepository();
+            //echo($carpoolOfferRepository->getCarpoolOffers());
 
-            // $carpoolOffer = new CarpoolOffer(
-            //     $user2Id,
-            //     '51995258424',
-            //     'Charqueadas',
-            //     'Centro',
-            //     'Patricio Ferreira',
-            //     'Charqueadas',
-            //     'Centro',
-            //     'Rua de tal',
-            //     '2021-03-31 10:15:00',
-            //     '2021-03-31 10:30:00',
-            //     4
-            // );
-
-            // $carpoolRequestRepository = new CarpoolRequestRepository();
-            // $carpoolRequestId = $carpoolRequestRepository->createCarpoolRequest($carpoolRequest);
-            
-            // $carpoolOfferRepository = new CarpoolOfferRepository();
-            // $carpoolOfferId = $carpoolOfferRepository->createCarpoolOffer($carpoolOffer);
-            
-            // $carpoolMatch = new CarpoolMatch(
-            //     $carpoolRequestId,
-            //     $carpoolOfferId
-            // );
-
-            // $carpoolMatchRepository = new CarpoolMatchRepository();
-            // $result = $carpoolMatchRepository->createCarpoolMatch($carpoolMatch);
+            $carpoolMatchRepository = new CarpoolMatchRepository();
+            //echo($carpoolMatchRepository->getCarpoolMatchById(1));
         ?>
     </body>
 </html>
