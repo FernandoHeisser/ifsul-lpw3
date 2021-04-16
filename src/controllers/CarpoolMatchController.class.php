c4f6e8a74c5d85e42c8e33c7d5e57a2aed8f99a1
Fernando Heisser <fernando_heisser@dellteam.com>
2021-04-16 17:23:31 -0300

    CarpoolMatchController refactor

--- src/controllers/CarpoolMatchController.class.php
+++ src/controllers/CarpoolMatchController.class.php
@@ -12,12 +12,15 @@
         public static function getCarpoolMatchById($id) {
             return self::$carpoolMatchRepository->getCarpoolMatchById($id);
         }
+
         public static function getCarpoolMatchsByCarpoolOfferId($carpoolOfferId) {
             return self::$carpoolMatchRepository->getCarpoolMatchsByCarpoolOfferId($carpoolOfferId);
         }
+
         public static function getCarpoolMatchsByCarpoolRequestId($carpoolRequestId) {
             return self::$carpoolMatchRepository->getCarpoolMatchsByCarpoolRequestId($carpoolRequestId);
         }
+        
         public static function getCarpoolMatchsByCarpoolOfferIdAndCarpoolRequestId($carpoolOfferId, $carpoolRequestId) {
             return self::$carpoolMatchRepository->getCarpoolMatchsByCarpoolOfferIdAndCarpoolRequestId($carpoolOfferId, $carpoolRequestId);
         }
