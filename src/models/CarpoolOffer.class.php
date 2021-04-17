3c1cd11d95e15e6e963c063fd989de8d5796850c
Fernando Heisser <fernando_heisser@dellteam.com>
2021-04-16 21:35:20 -0300

    abstract class added

--- src/models/CarpoolOffer.class.php
+++ src/models/CarpoolOffer.class.php
@@ -1,89 +1,23 @@
 <?php
     include_once("interfaces/ICarpoolOffer.php");
+    include_once("Carpool.class.php");
 
-    class CarpoolOffer implements ICarpoolOffer {
-        private $id;
-        private $userId;
-        private $phone;
-        private $fromCity;
-        private $fromNeighborhood;
-        private $fromStreet;
-        private $toCity;
-        private $toNeighborhood;
-        private $toStreet;
-        private $starDate;
-        private $endDate;
+    class CarpoolOffer extends Carpool implements ICarpoolOffer {
         private $availableVacancies;
-        private $done = false;
-        private $canceled = false;
 
-        function __construct($userId, $phone, $fromCity, $fromNeighborhood, $fromStreet, $toCity, $toNeighborhood, $toStreet, $starDate, $endDate, $availableVacancies) {
-            $this->userId = $userId;
-            $this->phone = $phone;
-            $this->fromCity = $fromCity;
-            $this->fromNeighborhood = $fromNeighborhood;
-            $this->fromStreet = $fromStreet;
-            $this->toCity = $toCity;
-            $this->toNeighborhood = $toNeighborhood;
-            $this->toStreet = $toStreet;
-            $this->starDate = $starDate;
-            $this->endDate = $endDate;
+        function __construct($userId, $phone, $fromCity, $fromNeighborhood, $fromStreet, $toCity, $toNeighborhood, $toStreet, $startDate, $endDate, $availableVacancies) {
+            parent::__construct($userId, $phone, $fromCity, $fromNeighborhood, $fromStreet, $toCity, $toNeighborhood, $toStreet, $startDate, $endDate);
             $this->availableVacancies = $availableVacancies;
         }
 
-        public function getId() {
-            return $this->id;
-        }
-        public function getUserId() {
-            return $this->userId;
-        }
-        public function getPhone() {
-            return $this->phone;
-        }
-        public function getFromCity() {
-            return $this->fromCity;
-        }
-        public function getFromNeighborhood() {
-            return $this->fromNeighborhood;
-        }
-        public function getFromStreet() {
-            return $this->fromStreet;
-        }
-        public function getToCity() {
-            return $this->toCity;
-        }
-        public function getToNeighborhood() {
-            return $this->toNeighborhood;
-        }
-        public function getToStreet() {
-            return $this->toStreet;
-        }
-        public function getStartDate() {
-            return $this->starDate;
-        }
-        public function getEndDate() {
-            return $this->endDate;
-        }
-        public function getDone() {
-            return $this->done;
-        }
-        public function getCanceled() {
-            return $this->canceled;
-        }
         public function getAvailableVacancies() {
             return $this->availableVacancies;
-        }
-
-        public function setDone() {
-            $this->done = true;
-        }
-        public function setCanceled() {
-            $this->canceled = true;
         }
 
         public function addAvailableVacancies() {
             $this->availableVacancies++;
         }
+
         public function removeAvailableVacancies() {
             $this->availableVacancies--;
         }
--- src/models/CarpoolRequest.class.php
+++ src/models/CarpoolRequest.class.php
@@ -1,79 +1,11 @@
 <?php
     include_once("interfaces/ICarpoolRequest.php");
+    include_once("Carpool.class.php");
 
-    class CarpoolRequest implements ICarpoolRequest {
-        private $id;
-        private $userId;
-        private $phone;
-        private $fromCity;
-        private $fromNeighborhood;
-        private $fromStreet;
-        private $toCity;
-        private $toNeighborhood;
-        private $toStreet;
-        private $startDate;
-        private $endDate;
-        private $done = false;
-        private $canceled = false;
+    class CarpoolRequest extends Carpool implements ICarpoolRequest {
 
         function __construct($userId, $phone, $fromCity, $fromNeighborhood, $fromStreet, $toCity, $toNeighborhood, $toStreet, $startDate, $endDate) {
-            $this->userId = $userId;
-            $this->phone = $phone;
-            $this->fromCity = $fromCity;
-            $this->fromNeighborhood = $fromNeighborhood;
-            $this->fromStreet = $fromStreet;
-            $this->toCity = $toCity;
-            $this->toNeighborhood = $toNeighborhood;
-            $this->toStreet = $toStreet;
-            $this->startDate = $startDate;
-            $this->endDate = $endDate;
-        }
-
-        public function getId() {
-            return $this->id;
-        }
-        public function getUserId() {
-            return $this->userId;
-        }
-        public function getPhone() {
-            return $this->phone;
-        }
-        public function getFromCity() {
-            return $this->fromCity;
-        }
-        public function getFromNeighborhood() {
-            return $this->fromNeighborhood;
-        }
-        public function getFromStreet() {
-            return $this->fromStreet;
-        }
-        public function getToCity() {
-            return $this->toCity;
-        }
-        public function getToNeighborhood() {
-            return $this->toNeighborhood;
-        }
-        public function getToStreet() {
-            return $this->toStreet;
-        }
-        public function getStartDate() {
-            return $this->startDate;
-        }
-        public function getEndDate() {
-            return $this->endDate;
-        }
-        public function getDone() {
-            return $this->done;
-        }
-        public function getCanceled() {
-            return $this->canceled;
-        }
-
-        public function setDone() {
-            $this->done = true;
-        }
-        public function setCanceled() {
-            $this->canceled = true;
+            parent::__construct($userId, $phone, $fromCity, $fromNeighborhood, $fromStreet, $toCity, $toNeighborhood, $toStreet, $startDate, $endDate);
         }
     }
 ?>
